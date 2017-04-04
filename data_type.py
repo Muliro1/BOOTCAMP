e38e36e0e6430e0e269b8237b2aaf9664a0cd6a2
Muliro1 <mulirokhaemba@gmail.com>
2017-04-04 15:00:40 +0300

    data_type function

---
+++ data_type.py
@@ -0,0 +1,19 @@
+def data_type(x):
+	if isinstance(x, str):
+		return len(x)
+	elif x == None:
+		return 'no value'
+	elif x == True or x == False:
+		return x
+	elif isinstance(x, int):
+		if x > 100:
+			return 'more than 100'
+		elif x < 100:
+			return 'less than 100'
+		else:
+			return 'equal to 100'
+	elif isinstance(x, list):
+		if len(x) >= 3:
+			return x[2]
+		else:
+			return None
