2e2542d63bd268b55658bd89d2d653aa765b5886
Muliro1 <mulirokhaemba@gmail.com>
2017-10-27 03:26:05 -0700

    Update CarClass.py
    added a getter method to get the full car description

--- CarClass.py
+++ CarClass.py
@@ -2,4 +2,11 @@ class CarClass(object):
 	def __init__(self, type_, model, name):
 		self.type_ = type_
 		self.model = model
-		self.name = name
+		self.name = name 
+        def get_type(self):
+		return 'the type is {} {} {}'.format(self.type_, self.model, self.name)
+	
+	
+	
+	
+       
